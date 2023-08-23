64354541006c6d175aa2b6bde1f553e37cf0c192
Suaveeb <138173179+Suaveeb@users.noreply.github.com>
2023-08-22 20:41:31 -0400

    Add files via upload

---
+++ form.php
@@ -0,0 +1,12 @@
+<form action="traitement.php" method="post">
+    <label for="nom">Nom :</label><br>
+    <input type="text" id="nom" name="nom" required><br><br>
+    
+    <label for="email">Email :</label><br>
+    <input type="email" id="email" name="email" required><br><br>
+    
+    <label for="mot_de_passe">Mot de passe :</label><br>
+    <input type="password" id="mot_de_passe" name="mot_de_passe" required><br><br>
+    
+    <input type="submit" value="S'inscrire">
+</form>
